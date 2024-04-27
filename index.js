93f910641fbc17488f0fd92ba1707151a8ba5f86
Michael Castro <michael.castro709@gmail.com>
2024-04-27 18:01:24 -0400

    Add the full to-do list functionality & some styling

--- index.js
+++ index.js
@@ -3,15 +3,44 @@ let userInput = document.getElementById("user-input");
 const myForm = document.getElementById("my-form")
 const submitButton = document.getElementById("submit-btn")
 const listContainer = document.getElementById("list-container")
+const finishedTask = document.getElementById('finished-container')
+const tasks = document.querySelectorAll(".task")
 
-//stops the page from refreshing when submit form
+//stops the page from refreshing when submit form is clicked
 myForm.addEventListener('submit', function(event){
     event.preventDefault();
 })
 
-//logs the users input when the sumbit button is clicked
+//adds a task to the todo list
 submitButton.addEventListener("click", (event) => {
-    if(userInput.value != ''){
-        console.log(userInput.value)
-    }
+    addTask();
 })
+
+//adds the created task to the to do list
+function addTask(){
+    if(userInput.value !== ''){
+        let listItem = document.createElement('li');
+        let itemtext = document.createTextNode(userInput.value)
+        listItem.appendChild(itemtext);
+
+        listContainer.appendChild(listItem)
+        listItem.classList.add("to-do")
+
+        userInput.value = '';//removes user input from box
+
+        
+        //if task is clicked in tasks section, it will move to the completed section & vice versa
+        listItem.addEventListener("click", function(){
+            listItem.classList.toggle("completed")
+
+            if(listItem.classList.contains("completed")){
+                finishedTask.appendChild(listItem);
+            }
+            else{
+                listContainer.appendChild(listItem);
+            }
+       })
+    }
+}
+
+
